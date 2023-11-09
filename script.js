549886dcb05969afa3d9cd96cdc8198152188548
Patrick <50429647+patrickmblack@users.noreply.github.com>
2023-11-09 14:10:57 -0800

    Add basic grid layout and cards
    Grid layout and cards added, will refine layout

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const myLibrary = [] // Array of book objects
+
+
 function Book(title, author, pageNumber, readBool){
     this.title = title;
     this.author = author;
@@ -13,6 +16,29 @@ function Book(title, author, pageNumber, readBool){
     }
 }
 
-const bookTheHobbit = new Book('The Hobbit','J.R.R. Tolkein', 150, true)
+function addBookToLibrary(book){
+    if (myLibrary.includes(book) === false){
+        myLibrary.push(book)
+    }else{
+        console.log("Book exists")
+    }
+}
 
-console.log(bookTheHobbit.info())
+const bookTheHobbit = new Book('The Hobbit','J.R.R. Tolkein', 310, false)
+const bookFellowship = new Book('The Fellowship of the Ring','J.R.R. Tolkein', 423, false)
+const bookTwoTowers = new Book('The Two Towers','J.R.R. Tolkein',352,false)
+const bookReturnKing = new Book('The Return of the King','J.R.R. Tolkein',416,false)
+const bookMartian = new Book('The Martian','Andy Wier',369, true)
+
+
+
+// console.log(bookTheHobbit.info())
+console.log(myLibrary)
+
+addBookToLibrary(bookTheHobbit)
+addBookToLibrary(bookFellowship)
+addBookToLibrary(bookTwoTowers)
+addBookToLibrary(bookReturnKing)
+addBookToLibrary(bookMartian)
+
+console.log(myLibrary)
